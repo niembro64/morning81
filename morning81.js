56d6e9320fc85965167e7a1d6086e96f0a9785bd
=Eric Niemeyer <=niemeyer.eric@gmail.com>
2022-03-14 11:03:19 -0400

    done

--- morning81.js
+++ morning81.js
@@ -1,12 +1,32 @@
 const stringToWordArray = (str) => {
+    word = "";
+    newArr = [];
 
+    for (var i = 0; i < str.length; i++) {
+        if (str[i] === " " && word.length != 0) {
+            newArr.push(word);
+            word = "";
+        } else if (str[i] != " ") {
+            word += str[i];
+        }
+    }
 
+    if (word.length != 0) {
+        newArr.push(word);
+    }
 
-
-}
+    return newArr;
+};
 
 const reverseWordOrder = (str) => {
+    var newArr = stringToWordArray(str);
+    var sentence = "";
+    for (var i = 0; i < newArr.length; i++) {
+        sentence = newArr[i] + " " + sentence;
+    }
+    return sentence;
+};
 
-}
-
-console.log(stringToWordArray("Did I shine my shoes today?"));
+string = "Did I shine my shoes today?";
+// console.log(stringToWordArray(string));
+console.log(reverseWordOrder(string));
